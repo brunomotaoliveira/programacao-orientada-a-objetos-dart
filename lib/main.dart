ed75f3fc0ee64cf7bedf800967f95b6af415fa8a
brunomotadev <brunomotadev@gmail.com>
2021-06-04 12:04:37 -0300

    classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-
+import 'package:orientacao_objetos/src/pessoa.dart';
 
 void main(List<String> arguments) {
-  
+  var pessoa = Pessoa();
+  print(pessoa.nome);
+  print(pessoa.idade);
+  print(pessoa.sexo);
 }
---
+++ lib/src/pessoa.dart
@@ -0,0 +1,5 @@
+class Pessoa {
+  String nome = "Bruno Mota";
+  int idade = 29;
+  String sexo = "M";
+}
